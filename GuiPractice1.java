f8e8253bfb6ba06b13c207149590da7bc20a88bd
rpalesa59-vilakazi <rpalesa59@gmail.com>
2025-08-05 23:49:12 +0200

    Add files via upload

---
+++ GuiPractice1.java
@@ -0,0 +1,14 @@
+
+package guipractice1;
+
+import guis.GuiFrame;
+
+
+public class GuiPractice1 {
+
+    
+    public static void main(String[] args) {
+      GuiFrame fr = new GuiFrame();
+    }
+    
+}
